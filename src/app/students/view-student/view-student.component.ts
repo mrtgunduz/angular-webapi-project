ae1d5448a83ec3f5431b76e8b85ffab44066bd8f
mrtgunduz <mrt.gunduz25@gmail.com>
2023-07-17 16:23:48 +0300

    student detay sayfası hazırlandı

--- src/app/students/view-student/view-student.component.ts
+++ src/app/students/view-student/view-student.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
+import { Gender, StudentTypes } from 'src/app/core/models/student.model';
+import { GenderService } from 'src/app/core/services/gender-service';
 import { StudentService } from 'src/app/core/services/student.service';
 
 @Component({
@@ -8,25 +11,55 @@ import { StudentService } from 'src/app/core/services/student.service';
   styleUrls: ['./view-student.component.scss'],
 })
 export class ViewStudentComponent {
-  studentId?: string | null;
+  studentTypesId?: string | null;
+  genderList: Gender[] = []
+  student: StudentTypes = {
+    id: '',
+    firstName: '',
+    lastName: '',
+    dateOfBirth: '',
+    email: '',
+    mobile: 0,
+    profileImageUrl: '',
+    genderId: '',
+    gender: {
+      id: '',
+      description: '',
+    },
+    address: {
+      id: '',
+      physicalAddress: '',
+      postalAddress: '',
+      studentId: '',
+    },
+  };
   constructor(
     private studentService: StudentService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private genderService: GenderService
   ) {}
 
-  ngOnInit(): void {
-    this.route.paramMap.subscribe(
+  ngOnInit() {
+    this.getStudentDetails();
+    this.getGenderDetails();
+  }
 
-      (params) => {
-      this.studentId = params.get('id');
-      this.studentService.getStudent(this.studentId).subscribe(
-        (res) => {
+  getStudentDetails() {
+    this.route.paramMap.subscribe((params) => {
       debugger;
-        }
-      )
-    }
+      this.studentTypesId = params.get('id');
+      this.studentService
+        .getStudent(this.studentTypesId)
+        .subscribe((data) => {
+          this.student = data;
 
+        });
+    });
+  }
 
-    );
+  getGenderDetails(){
+    this.genderService.getGendersList().subscribe((data) => {
+      this.genderList = data;
+    })
   }
 }
